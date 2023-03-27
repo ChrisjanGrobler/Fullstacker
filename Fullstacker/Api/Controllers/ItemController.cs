9b8c082dfd7500b829698705c6def177c9f10804
kevinmalan <kmalan@outlook.com>
2023-03-27 20:50:47 +0200

    Created an API endpoint that allows a user to create an item.

---
+++ Fullstacker/Api/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class CreateItemDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
+    {
+        /// <summary>
+        /// An endpoint for creating an item
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create([FromBody] CreateItemDto request)
+        {
+            return Ok(); // HTTP Status Code 200
+        }
+    }
+}
