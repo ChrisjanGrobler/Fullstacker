63aba6db6d6c0a0d2abe7c158a9518a92d3727cc
kevinmalan <kmalan@outlook.com>
2023-03-27 21:03:03 +0200

    Extended ItemController

--- Fullstacker/Api/Controllers/ItemController.cs
+++ Fullstacker/Api/Controllers/ItemController.cs
@@ -1,16 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class CreateItemDto
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-
     [Route("api/[controller]")]
     public class ItemController : ControllerBase
     {
+        /// <summary>
+        /// An endpoint for retrieving a list of items
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("get")]
+        public IActionResult Get()
+        {
+            var items = new List<GetItemDto>
+            {
+                new GetItemDto
+            {
+                Id = 1,
+                Name = "Lip Ice",
+                Description = "Self explanatory"
+            },
+                new GetItemDto
+                {
+                Id = 2,
+                Name = "Dog Food",
+                Description = ""
+                }
+            };
+
+            return Ok(items);
+        }
+
+        /// <summary>
+        /// An endpoint for retrieving an item by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("get/{id}")]
+        public IActionResult Get(int id)
+        {
+            var item = new GetItemDto
+            {
+                Id = 1,
+                Name = "Lip Ice",
+                Description = "Self explanatory"
+            };
+
+            return Ok(item);
+        }
+
         /// <summary>
         /// An endpoint for creating an item
         /// </summary>
---
+++ Fullstacker/Api/Dtos/CreateItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Dtos
+{
+    public class CreateItemDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
